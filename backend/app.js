7aea5f727e4c1e74171f0708bb3714718c207a7b
AlabiAbdullah1 <147815251+AlabiAbdullah1@users.noreply.github.com>
2025-08-16 13:31:40 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT||8800;
 const app = express()
 
 app.use(cors({
-  origin: ["https://campus-guard.vercel.app/", "http://localhost:5173", "http://localhost:5174"],
+  origin: ["https://campus-guard.vercel.app", "http://localhost:5173", "http://localhost:5174"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
